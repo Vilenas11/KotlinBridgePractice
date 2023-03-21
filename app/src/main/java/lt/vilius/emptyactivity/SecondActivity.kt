0a0cc7127b140740d33f2d9b896b500013d415ee
Vilenas11 <78947355+Vilenas11@users.noreply.github.com>
2023-03-21 14:07:23 +0200

    SecondActivity add button, textView and background color

--- app/src/main/java/lt/vilius/emptyactivity/SecondActivity.kt
+++ app/src/main/java/lt/vilius/emptyactivity/SecondActivity.kt
@@ -2,10 +2,18 @@ package lt.vilius.emptyactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val secondActivityButton : Button = findViewById(R.id.buttonOpenSecond)
+        val secondActivityTextView : TextView = findViewById(R.id.textViewSecond)
+        secondActivityButton.visibility = View.VISIBLE
+
+
     }
 }
